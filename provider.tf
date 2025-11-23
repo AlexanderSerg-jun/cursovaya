d7eaa5ac2ddea706aeaf11c341d9bcfb4904f5b3
SavinAS <savinas@gpbl.ru>
2025-11-23 08:24:30 +0300

    Create folder and some files for project

---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "yandex"{
+    token = var.token
+    zone = var.zone_1
+
+}
---
+++ virables.tf
@@ -0,0 +1,28 @@
+variable token {
+  type        = string
+  default     = ""
+  description = "token_id_yandex"
+}
+variable cloud_id {
+  type        = string
+  default     = ""
+  description = "cloud_id_yandex"
+}
+variable  folder_id{
+  type        = string
+  default     = ""
+  description = "folder_id_yandex"
+}
+variable zone_1 {
+  type        = string
+  default     = ""
+  description = "description"
+}
+variable zone_2 {
+  type        = string
+  default     = ""
+  description = "description"
+}
+
+
+
