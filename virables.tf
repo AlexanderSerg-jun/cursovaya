552e4a1d4d582358aedab5e9d9eb7c3d52e5ac51
SavinAS <savinas@gpbl.ru>
2025-11-23 08:32:07 +0300

    Create folder and some files for project

--- virables.tf
+++ virables.tf
@@ -1,28 +1,14 @@
-variable token {
-  type        = string
-  default     = ""
-  description = "token_id_yandex"
+# Задаем переменную access 
+variable "access" {
+  type = map(any)
+  defualt = {
+    token =""
+    cloud_id = ""
+    folder_id = ""
+    zone =""
+  }
 }
-variable cloud_id {
-  type        = string
-  default     = ""
-  description = "cloud_id_yandex"
-}
-variable  folder_id{
-  type        = string
-  default     = ""
-  description = "folder_id_yandex"
-}
-variable zone_1 {
-  type        = string
-  default     = ""
-  description = "description"
-}
-variable zone_2 {
-  type        = string
-  default     = ""
-  description = "description"
-}
+
 
 
 
